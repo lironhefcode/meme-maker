c4a72907cd96c100d52a00acb78bfc53eef3d0f1
lironhefcode <lironheftman@gmail.com>
2025-01-09 00:57:31 +0200

    added nav

--- js/gallarey-controler.js
+++ js/gallarey-controler.js
@@ -1,11 +1,32 @@
 
-
+gCurrNav =  document.querySelector('.gallery')
+gCurrPage =  document.querySelector('.image-gallery')
 
 
 function onClickImage(img){
+    
     document.querySelector('.image-gallery').classList.add('hide')
     document.querySelector('.meme-editor').classList.remove('hide')
+    gCurrPage = document.querySelector('.meme-editor')
     selectedImg(img.src)
     sizeCanvas()
     rendermeme()
+}
+function toggleMenu(){
+    document.body.classList.toggle('menu-open')
+}
+function chnageMode(el){
+    
+    gCurrNav.classList.remove('active')
+    el.classList.add('active')
+    gCurrNav = el
+}
+function goToGallery(){
+    const elGallery  =document.querySelector('.image-gallery')
+    gCurrPage.classList.add('hide')
+    elGallery.classList.remove('hide')
+    gCurrPage = elGallery
+}
+function goToEditor(){
+
 }
